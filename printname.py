0de6106e31e19c0d212c2a7a4b018526e4b2c794
rakesh <rakeshvngowda@gmail.com>
2020-08-09 17:26:36 +0530

    jjdk

---
+++ printname.py
@@ -0,0 +1,10 @@
+def name():
+    name = 'rakesh'
+    print('my name is', name)
+def soda():
+    print('i can drink soda')
+def water():
+    print('i can drink water')
+def beer():
+    print('i can drink beer')
+    
