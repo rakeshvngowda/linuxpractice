3fe440dade23eff9fbaf9c5e9af3e061d392178d
rakesh <rakeshvngowda@gmail.com>
2020-08-17 23:29:19 +0530

    this is d

--- printname.py
+++ printname.py
@@ -1,12 +1,1 @@
-class IntValueError(Exception):
-    def __init__(self, msg):
-        self.msg = msg
-        
-
-a = 10
-b =2
-
-if type(int(((a/b)**0.2)**2)) != int:
-    raise IntValueError('result is not in integer')
-else:
-    print(a/b)
+# this is program
